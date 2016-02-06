d93c6f876741c4cfa44af8fd0ac48837e7874669
zuyanliang@gmail.com <zuyanliang@gmail.com>
2016-02-06 01:03:29 -0500

    WIP map

--- app/js/controllers/MainCtrl.js
+++ app/js/controllers/MainCtrl.js
@@ -19,11 +19,23 @@ angular.module('nuageInvestment').controller('MainCtrl', ['$scope', 'LanguageDat
             zoom: 17
         };
         
+        var label = {
+            address : 'Nuage Investment Realty',
+            anchor: '-15 12',
+            style: 'marker-labels'
+        } 
+
         $scope.marker = {
             id: 0,
             coords : {
                 latitude: 42.349445,
                 longitude: -71.222065
+            },
+            options: {
+                labelContent: label.address,
+                labelAnchor: label.anchor,
+                labelClass: label.style,
+                labelVisible: true
             }
         };
 
